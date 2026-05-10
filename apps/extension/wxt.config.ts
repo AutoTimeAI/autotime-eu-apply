be9c8fdc91fc1ddcb23cde8fb8abf266368a3f1f
DataByRajesh <rajeshkumaratuk@gmail.com>
2026-05-10 14:59:12 +0100

    Fix extension icon and panel grid

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -11,7 +11,19 @@ export default defineConfig({
       matches: ["https://autotime-eu-apply.vercel.app/*"]
     },
     action: {
+      default_icon: {
+        "16": "icons/16.png",
+        "32": "icons/32.png",
+        "48": "icons/48.png",
+        "128": "icons/128.png"
+      },
       default_title: "AutoTime EU Apply"
+    },
+    icons: {
+      "16": "icons/16.png",
+      "32": "icons/32.png",
+      "48": "icons/48.png",
+      "128": "icons/128.png"
     }
   }
 })
