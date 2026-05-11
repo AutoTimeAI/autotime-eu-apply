976bd2441aca601ab1fadfb887b24911207ba15c
DataByRajesh <rajeshkumaratuk@gmail.com>
2026-05-11 09:25:08 +0100

    Add supported job board host permissions

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -1,12 +1,39 @@
 import { defineConfig } from "wxt"
 
+const supportedJobBoardHostPermissions = [
+  "https://*.stepstone.de/*",
+  "https://*.stepstone.com/*",
+  "https://*.indeed.com/*",
+  "https://*.indeed.co.uk/*",
+  "https://eures.europa.eu/*",
+  "https://*.eurotechjobs.com/*",
+  "https://*.eurojobs.com/*",
+  "https://*.nextleveljobs.eu/*",
+  "https://*.wellfound.com/*",
+  "https://*.angel.co/*",
+  "https://*.xing.com/*",
+  "https://*.welcometothejungle.com/*",
+  "https://*.nationalevacaturebank.nl/*",
+  "https://*.infojobs.net/*",
+  "https://*.infojobs.it/*",
+  "https://*.monster.com/*",
+  "https://*.monster.co.uk/*",
+  "https://*.monster.de/*",
+  "https://*.monster.fr/*",
+  "https://*.eurotoptech.com/*",
+  "https://*.jobteaser.com/*"
+]
+
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   manifest: {
     name: "AutoTime EU Apply",
     description: "Cross-border job application copilot for Europe",
     permissions: ["activeTab", "scripting", "storage"],
-    host_permissions: ["https://autotime-eu-apply.vercel.app/*"],
+    host_permissions: [
+      "https://autotime-eu-apply.vercel.app/*",
+      ...supportedJobBoardHostPermissions
+    ],
     externally_connectable: {
       matches: ["https://autotime-eu-apply.vercel.app/*"]
     },
